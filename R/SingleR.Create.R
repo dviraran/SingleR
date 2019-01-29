0bd9d93515d996a0d893aac039194e65ab9c9305
Alexandru Ioan Voda <alexandruioanvoda@users.noreply.github.com>
2019-01-29 15:40:08 +0000

    Update SingleR.Create.R

--- R/SingleR.Create.R
+++ R/SingleR.Create.R
@@ -457,7 +457,7 @@ CreateSinglerObject = function(counts,annot=NULL,project.name,
   singler$singler = res
   
   if (do.signatures==TRUE) {
-    signatures = calculateSignatures(sc.data.gl,species=species)
+    signatures = calculateSignatures(sc.data.gl,species=species,numCores=SingleR.numCores)
     singler$signatures = signatures
     
   }
